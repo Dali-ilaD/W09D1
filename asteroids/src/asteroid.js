904ee9953f23db5c117ec1315cbd5c58fa476cdc
Brian Vo <brianhvo02@gmail.com>
2023-03-27 15:50:05 -0700

    Asteroid partially done

--- asteroids/src/asteroid.js
+++ asteroids/src/asteroid.js
@@ -1,1 +1,18 @@
-class Aste
+import MovingObject from "./moving_object";
+import * as Util from "./util.js";
+
+class Asteroid extends MovingObject{
+    static RADIUS = 25;
+    static COLOR = '#808080';
+    constructor(options){
+
+        
+        options.color = Asteroid.COLOR;
+        options.radius = Asteroid.RADIUS;
+        options.vel = Util.randomVec()
+        super(options)
+    }
+}
+
+
+export default Asteroid;
