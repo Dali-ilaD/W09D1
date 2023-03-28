5128ebfeb9945f41adb828b4160ff73c6e88566d
Brian Vo <brianhvo02@gmail.com>
2023-03-27 17:30:17 -0700

    gameview

--- asteroids/src/game.js
+++ asteroids/src/game.js
@@ -1,3 +1,42 @@
+import Asteroid from "./asteroid";
+import randomVec from "./utils";
+
 class Game {
-    static:
+    static DIM_X = 600;
+    static DIM_Y = 300;
+    static NUM_ASTEROIDS = 7
+    constructor(){
+        this.asteroids = this.addAsteroids
+
+    }
+    addAsteroids(){
+        const asts = []
+        for (let i = 0; i <= NUM_ASTEROIDS; i++) {
+            pos = [this.randomPosition(600), this.randomPosition(300)];
+            let ast = new Asteroid(pos);
+            asts.push(ast);
+        }
+        return asts;
+    }
+    
 }
+
+Game.prototype.randomPosition = function(max){
+    return Math.random() * max;
+}
+
+
+Game.prototype.draw= function(ctx) {
+    clearRect(0,0,600,300);
+    this.asteroids.forEach(asteroid => {
+        asteroid.draw(ctx);
+    });
+}
+
+Game.prototype.moveObjects = function() {
+    this.asteroids.forEach(asteroid =>{
+        asteroid.move();
+    });
+}
+
+
---
+++ asteroids/src/game_view.js
@@ -0,0 +1,10 @@
+class GameView {
+    constructor(ctx){
+        this.game = game 
+        this.ctx = ctx
+    }
+}
+
+GameView.prototype.start = function() {
+    
+}
